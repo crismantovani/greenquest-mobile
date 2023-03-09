6a88c72ffa5a60ec35b8f2bc18a98d65157e8d1c
Amanda Santos <amd.mantovani@gmail.com>
2023-03-08 21:37:20 -0300

    CRIA a searchbar na página home e adiciona INTENT das pACTIVITIES

--- app/src/main/java/br/com/greenquest/SignUp.kt
+++ app/src/main/java/br/com/greenquest/SignUp.kt
@@ -1,11 +1,21 @@
 package br.com.greenquest
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val btnRegister = findViewById<Button>(R.id.buttonRegister)
+
+        btnRegister.setOnClickListener {
+            val i = Intent(this, Home::class.java)
+            startActivity(i)
+
+        }
     }
 }
